1e382745544eea885276160843c9b9f3f15057ee
Victor Del Carpio <vhdelcg@gmail.com>
2024-05-02 22:55:10 -0400

    feat: updated main to show how to use.

--- main.c
+++ main.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include "openai.h"
 
-int main(void) {
-    OpenAI* openai = createOpenAI("OPENAIKEY", "Personal", "Default Project");
-    Response* response = chat(openai, "gpt-3.5-turbo-0125", "Hello world", 0.7);
+int main(void)
+{
+    Client *openai = OpenAI("MY-API-KEY", "Personal", "Default Project");
+    Response *response = chat(openai, "gpt-3.5-turbo-0125", "Hello world", 0.7);
+    if (response == NULL) {
+        destroyClient(openai);
+        return EXIT_FAILURE;
+    }
 
     printf("%s\n", response->id);
     printf("%s\n", response->object);
     printf("%s\n", response->model);
-    printf("%s\n", response->message_content);
-    printf("%d\n", response->prompt_tokens);
+    printf("%s\n", response->choices[0]->message->content);
     printf("%s\n", response->system_fingerprint);
+    printf("%d\n", response->usage->completion_tokens);
 
     destroyResponse(response);
+    destroyClient(openai);
     return EXIT_SUCCESS;
 }
-
